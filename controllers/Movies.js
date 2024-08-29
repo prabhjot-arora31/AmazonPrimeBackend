307f7bc2358eeacb7e6c3693c7b575da171924f5
unknown <prabhjotarora99@gmail.com>
2024-08-29 23:51:26 +0530

    adding all the movies that are available

--- controllers/Movies.js
+++ controllers/Movies.js
@@ -1,7 +1,10 @@
 import asynHandler from "express-async-handler";
 import fs from "fs";
-import data2 from '../data/adventure_movies.json' assert {type : "json" }
+import data2 from "../data/adventure_movies.json" assert { type: "json" };
 import data from "../data/animation_movies.json" assert { type: "json" };
+import data3 from "../data/crime_movies.json" assert { type: "json" };
+import data4 from "../data/horror_movies.json" assert { type: "json" };
+import data5 from "../data/romance_movies.json" assert { type: "json" };
 export const getMovies = asynHandler(async (req, res) => {
-  res.json([data,data2]);
+  res.json([data, data2, data3, data4, data5]);
 });
