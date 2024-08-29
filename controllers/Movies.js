b931a6d4c5fa3b6914fec372fa9c02d778a01039
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-08-29 23:18:59 +0530

    Update Movies.js sending romance movies

--- controllers/Movies.js
+++ controllers/Movies.js
@@ -1,6 +1,6 @@
 import asynHandler from "express-async-handler";
 import fs from "fs";
-import data from "../data/adventure_movies.json" assert { type: "json" };
+import data from "../data/romance_movies.json" assert { type: "json" };
 export const getMovies = asynHandler(async (req, res) => {
   res.json(data);
 });
