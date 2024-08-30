f67a462b2bc3f3ef09e7e36deeda96deba5d1120
unknown <prabhjotarora99@gmail.com>
2024-08-30 18:15:04 +0530

    removing all the movies to fetch to reduce bandwidth

--- controllers/Movies.js
+++ controllers/Movies.js
@@ -1,10 +1,9 @@
 import asynHandler from "express-async-handler";
-import fs from "fs";
-import data2 from "../data/adventure_movies.json" assert { type: "json" };
-import data from "../data/animation_movies.json" assert { type: "json" };
-import data3 from "../data/crime_movies.json" assert { type: "json" };
-import data4 from "../data/horror_movies.json" assert { type: "json" };
-import data5 from "../data/romance_movies.json" assert { type: "json" };
+// import data2 from "../data/adventure_movies.json" assert { type: "json" };
+// import data from "../data/animation_movies.json" assert { type: "json" };
+// import data3 from "../data/crime_movies.json" assert { type: "json" };
+// import data4 from "../data/horror_movies.json" assert { type: "json" };
+// import data5 from "../data/romance_movies.json" assert { type: "json" };
 export const getMovies = asynHandler(async (req, res) => {
-  res.json([data, data2, data3, data4, data5]);
+  res.json([]);
 });
